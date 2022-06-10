6f9dc3f8a604665b3faf209736a44211b9124ba4
Tahier Hussain <tahierhussain2205@gmail.com>
2022-06-10 20:24:52 +0530

    Bug Fix in Test API

--- routes/test.js
+++ routes/test.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const testController = require("../controllers/test");
 
-router.post("/", testController.test);
+router.get("/", testController.test);
 
 module.exports = router;
